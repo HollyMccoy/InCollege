5d4c26468be9be928fcfb0e66243334ddf0f2199
Logan <metzenlogan@gmail.com>
2020-09-20 18:23:01 -0400

    Restructured tests

--- InCollege_test.py
+++ InCollege_test.py
@@ -1,4 +1,20 @@
 from InCollege import ValidatePassword
 
-def test_ValidatePassword():
-    assert(ValidatePassword("example") == False)
+class TestValidatePassword:
+    def test_ValidatePasswordTooShort(self):
+        assert ValidatePassword("example") == False
+
+    def test_ValidatePasswordTooLong(self):
+        assert ValidatePassword("example123456") == False
+
+    def test_ValidatePasswordNoAlphanum(self):
+        assert ValidatePassword("Example12") == False
+
+    def test_ValidatePasswordNoCapitalLetter(self):
+        assert ValidatePassword("example12") == False
+
+    def test_ValidatePasswordNoNumber(self):
+        assert ValidatePassword("exampleee") == False
+
+    def test_ValidatePasswordHappyPath(self):
+        assert ValidatePassword("Example1!") == True
