e6dcb2fad8345880bfed1dba24d1eebac5da0af0
JMazzaro256 <71480898+JMazzaro256@users.noreply.github.com>
2020-09-25 18:41:46 -0400

    Add files via upload

--- User.py
+++ User.py
@@ -1,15 +1,19 @@
 #User object class
 class User:
-    def __init__ ( self, username ,  password ):     #constructor
+    def __init__(self, username, password, firstname, lastname):     #constructor
         self.username = username
         self.password = password
+        self.firstname = firstname
+        self.lastname = lastname
 
-
-    def CheckLogin (self, inputUser, inputPass):    #check login returns true if input is a valid account login, false if it is not a valid account
+    def CheckLogin(self, inputUser, inputPass):    #check login returns true if input is a valid account login, false if it is not a valid account
         if (self.username == inputUser):
             if (self.password == inputPass):
                 return True
         return False
 
     def Print(self):
-        return self.username + ' '+ self.password
+        return (self.username + ' '
+        + self.password + ' '
+        + self.firstname + ' '
+        + self.lastname)
