247d116fc291574b0e47db9b09ff6b2fc7d7b95f
JMazzaro256 <71480898+JMazzaro256@users.noreply.github.com>
2020-10-11 23:52:03 -0400

    Updated InCollege_test.py
    Included unit tests for main directory input files and "important links" subdirectory input files.

--- InCollege_test.py
+++ InCollege_test.py
@@ -1,5 +1,8 @@
 from InCollege import ValidatePassword, ValidateDate, ValidateDegree
 
+from _Tests import test_MainFiles  # Main directory files
+from _Tests import test_ImportantLinksFiles  # "Important links" submenu files
+
 class TestValidatePassword:
     def test_ValidatePasswordTooShort(self):
         assert ValidatePassword("example") == False
@@ -19,10 +22,11 @@ class TestValidatePassword:
     def test_ValidatePasswordHappyPath(self):
         assert ValidatePassword("Example1!") == True
 
+
 class TestValidateDegree:
     def test_ValidateDegree(self):
         assert ValidatePassword("Other Option that should be false") == False
-    
+
     def test_ValidateDegreeIncorrected(self):
         assert ValidatePassword("Example") == False
 
@@ -39,9 +43,8 @@ class TestValidateDegree:
         assert ValidatePassword("Doctorate") == True
 
 
-
 class TestValidateDate:
-    
+
     def test_ValidateDateInFormat(self):
         assert ValidatePassword("2020-12-10") == True
 
