8324e500c3f36fcf222adb1623d10616795128ec
JMazzaro256 <71480898+JMazzaro256@users.noreply.github.com>
2020-11-15 22:35:07 -0500

    Add / update test files

---
+++ testGlobals.py
@@ -0,0 +1,27 @@
+# Global variables and functions for unit test files
+
+def checkParamLength(fileName, numParams):
+    """Check for a specified number of file parameters"""
+    with open(fileName) as file:
+        while True:
+            params = file.readline().split()
+            if not params:  # Exit if at end-of-file
+                return True
+            elif len(params) == 0:  # Skip blank lines
+                continue
+            elif len(params) != numParams:
+                print("Incorrect number of parameters found in " + fileName)
+            return len(params) == numParams
+
+
+def checkIfEmpty(fileName):
+    """Check if a specified file is empty"""
+    with open(fileName) as file:
+        while True:
+            line = file.readline()
+            if not line:
+                return False
+            elif line == "\n":
+                return False
+            else:
+                return True
---
+++ test_NotificationFiles.py
@@ -0,0 +1,19 @@
+# Unit tests for "Notifications" subdirectory files
+from os import path
+
+fileDir = "docs\\Notifications\\"
+
+
+class TestNotificationFiles:
+    '''Check for files used by the "important links" submenus.'''
+    def test_ImportantLinksPath(self):
+        assert path.exists(fileDir)
+
+    def test_ApplyTimesFile(self):
+        assert path.exists(fileDir + "ApplyTimes.txt")
+
+    def test_DeletedJobsFile(self):
+        assert path.exists(fileDir + "DeletedJobs.txt")
+
+    def test_NewJobsFile(self):
+        assert path.exists(fileDir + "NewJobs.txt")
